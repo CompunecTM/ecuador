dc248f3ce5af39487ef8c3ea0eff4559b20b0fba
CompunecTM <compunecec@gmail.com>
2017-09-15 18:09:27 -0500

    Validar cedula y rud

--- controller/validacioncedrub.php
+++ controller/validacioncedrub.php
@@ -14,48 +14,57 @@ class validacioncedrub extends fs_controller
    {
       $this->share_extenstion();
 
-      if ($_POST['cedrud']) {
+       if (isset($_POST['cedrud'])) {
          $this->template = FALSE;
 
          $validador = new ValidarIdentificacion();
 
          $cedrud = trim($_POST['cedrud']);
 
-         $data['tipfiscal'] = $_POST['tipfiscal'];
+         if ($_POST['perfisica'] == 'true') {
+            // validar CI
+            $data['men'] =$_POST['perfisica'].' '.$_POST['tipfiscal'];
+            if ($_POST['tipfiscal'] == 'Cedula') {
+               if ($validador->validarCedula($cedrud)) {
+                  $data['mensaje'] = 'Cédula válida';
+                  $data['estatus'] = '1';
+               } else {
+                  $data['mensaje'] = 'Cédula incorrecta: '.$validador->getError();
+                  $data['estatus'] = '0';
+               }
+            }elseif ($_POST['tipfiscal'] == 'R.U.C') {
+               //validar RUC persona natural
+               if ($validador->validarRucPersonaNatural($cedrud)) {
+                  $data['mensaje'] = 'RUC válido';
+                  $data['estatus'] = '1';
+               } else {
+                  $data['mensaje'] ='RUC incorrecto: '.$validador->getError();
+                  $data['estatus'] = '0';
+               }
+            }
+           
+         }else{
 
-         $data['perfisica'] = $_POST['perfisica'];
-
-         // validar CI
-         if ($validador->validarCedula($cedrud)) {
-            $data['mensaje'] = 'Cédula válida';
-            $data['estatus'] = '1';
-         } else {
-            $data['mensaje'] = 'Cédula incorrecta: '.$validador->getError();
-            $data['estatus'] = '0';
+            if ($_POST['pribpubli'] == 'true') {
+               // validar RUC sociedad privada
+               if ($validador->validarRucSociedadPrivada($cedrud)) {
+                  $data['mensaje'] = 'RUC privado válido';
+                  $data['estatus'] = '1';
+               } else {
+                  $data['mensaje'] ='RUC privado incorrecto: '.$validador->getError();
+                  $data['estatus'] = '0';
+               }
+            }else{
+               // validar RUC sociedad publica
+               if ($validador->validarRucSociedadPublica($cedrud)) {
+                  $data['mensaje'] = 'RUC publico válido';
+                  $data['estatus'] = '1';
+               } else {
+                  $data['mensaje'] ='RUC publico incorrecto: '.$validador->getError();
+                  $data['estatus'] = '0';
+               }
+            }
          }
-
-         /* validar RUC persona natural
-         if ($validador->validarRucPersonaNatural('0926687856001')) {
-             echo 'RUC válido';
-         } else {
-             echo 'RUC incorrecto: '.$validador->getMessage();
-         } 1725030306
-
-         // validar RUC sociedad privada
-         if ($validador->validarRucSociedadPrivada('0992397535001')) {
-             echo 'RUC válido';
-         } else {
-             echo 'RUC incorrecto: '.$validador->getMessage();
-         }
-
-         // validar RUC sociedad ublica
-         if ($validador->validarRucSociedadPublica('1760001550001')) {
-             echo 'RUC válido';
-         } else {
-             echo 'RUC incorrecto: '.$validador->getMessage();
-         }*/
-
-          
          echo json_encode($data);
       }
 
@@ -66,14 +75,68 @@ class validacioncedrub extends fs_controller
 
    	$text = "<script type='text/javascript' src='plugins/ecuador/view/js/validacioncedrub.js'></script>";
 
-   	  /// metemos la pestaña de Numero de Autorizacion por Javascript
+   	  /// Validacion de cedula, rud a la pantalla del listado de clientes
       $fsext1 = new fs_extension();
-      $fsext1->name = 'validacioncedrub';
+      $fsext1->name = 'validacioncedrub1';
       $fsext1->from = __CLASS__;
       $fsext1->to = 'ventas_clientes';
       $fsext1->type = 'head';
       $fsext1->text = $text;
       $fsext1->save();
+
+        /// Validacion de cedula, rud a la pantalla cliente al editar datos del mismo
+      $fsext2 = new fs_extension();
+      $fsext2->name = 'validacioncedrub2';
+      $fsext2->from = __CLASS__;
+      $fsext2->to = 'ventas_cliente';
+      $fsext2->type = 'head';
+      $fsext2->text = $text;
+      $fsext2->save();
+
+      /// Validacion de cedula, rud a la pantalla cliente al editar datos del mismo
+      $fsext3 = new fs_extension();
+      $fsext3->name = 'validacioncedrub3';
+      $fsext3->from = __CLASS__;
+      $fsext3->to = 'nueva_venta';
+      $fsext3->type = 'head';
+      $fsext3->text = $text;
+      $fsext3->save();
+
+      /// Validacion de cedula, rud a la pantalla cliente al editar datos del mismo
+      $fsext4 = new fs_extension();
+      $fsext4->name = 'validacioncedrub4';
+      $fsext4->from = __CLASS__;
+      $fsext4->to = 'nuevo_servicio';
+      $fsext4->type = 'head';
+      $fsext4->text = $text;
+      $fsext4->save();
+
+      /// Validacion de cedula, rud a la pantalla cliente al editar datos del mismo
+      $fsext5 = new fs_extension();
+      $fsext5->name = 'validacioncedrub5';
+      $fsext5->from = __CLASS__;
+      $fsext5->to = 'nueva_compra';
+      $fsext5->type = 'head';
+      $fsext5->text = $text;
+      $fsext5->save();
+
+      /// Validacion de cedula, rud a la pantalla cliente al editar datos del mismo
+      $fsext6 = new fs_extension();
+      $fsext6->name = 'validacioncedrub6';
+      $fsext6->from = __CLASS__;
+      $fsext6->to = 'compras_proveedores';
+      $fsext6->type = 'head';
+      $fsext6->text = $text;
+      $fsext6->save();
+
+      /// Validacion de cedula, rud a la pantalla cliente al editar datos del mismo
+      $fsext7 = new fs_extension();
+      $fsext7->name = 'validacioncedrub7';
+      $fsext7->from = __CLASS__;
+      $fsext7->to = 'compras_proveedor';
+      $fsext7->type = 'head';
+      $fsext7->text = $text;
+      $fsext7->save();
    }
 
 }
--- view/js/validacioncedrub.js
+++ view/js/validacioncedrub.js
@@ -1,51 +1,186 @@
-function validar(thisaux,seltipfiscal){
+function validar(thisaux,campocedrud, camposeltipofiscal){
   $.ajax({
-  	url: 'index.php?page=validacioncedrub',
-  	type: 'POST',
-  	dataType: 'json',
-  	data: {cedrud: thisaux, tipfiscal: seltipfiscal, perfisica: $('input[name="personafisica"]').prop('checked')},
+    url: 'index.php?page=validacioncedrub',
+    type: 'POST',
+    dataType: 'json',
+    data: {cedrud: thisaux, tipfiscal: $('select[name="'+camposeltipofiscal+'"]').val(), perfisica: $('input[name="personafisica"]').prop('checked'),pribpubli: $('input[name="privpubli"]').prop('checked') },
   })
   .done(function(data) {
 
-  	$('input[name="cifnif"]').parent('div').removeClass('has-success');
-  	$('input[name="cifnif"]').parent('div').removeClass('has-error');
+    $('input[name="'+campocedrud+'"]').parent('div').removeClass('has-success');
+    $('input[name="'+campocedrud+'"]').parent('div').removeClass('has-error');
 
-  	if (data['estatus'] == '1') {
-  		$('input[name="cifnif"]').parent('div').addClass('has-success');
-  		$('button[type="submit"]').attr('disabled',false);
-  	}else{
-  		$('input[name="cifnif"]').parent('div').addClass('has-error');
-  		$('button[type="submit"]').attr('disabled',true);
-  	}
+    if (data['estatus'] == '1') {
+      $('input[name="'+campocedrud+'"]').parent('div').addClass('has-success');
+      $('button[type="submit"]').attr('disabled',false);
+    }else{
+      $('input[name="'+campocedrud+'"]').parent('div').addClass('has-error');
+      $('button[type="submit"]').attr('disabled',true);
+    }
 
-  	$('input[name="cifnif"]').attr('data-container', 'body');
-  	$('input[name="cifnif"]').attr('data-toggle', 'popover');
-  	$('input[name="cifnif"]').attr('data-placement', 'top');
-  	
-  	$('input[name="cifnif"]').attr('data-content', data['mensaje']);
+    $('input[name="'+campocedrud+'"]').attr('data-container', 'body');
+    $('input[name="'+campocedrud+'"]').attr('data-toggle', 'popover');
+    $('input[name="'+campocedrud+'"]').attr('data-placement', 'top');
+    
+    $('input[name="'+campocedrud+'"]').attr('data-content', data['mensaje']);
 
-  	$('input[name="cifnif"]').popover('show');
+    $('input[name="'+campocedrud+'"]').popover('show');
+
+
   })
   .fail(function() {
-  	console.log("error");
+    console.log("error");
   })
   .always(function() {
-  	setTimeout(function() {
-        $('input[name="cifnif"]').popover('hide');
+    setTimeout(function() {
+        $('input[name="'+campocedrud+'"]').popover('hide');
     }, 1000);
   });
   
 }
 
+function funvalidar(){
+
+	$("input[name='cifnif']").keyup(function() {
+		validar($(this).val(),'cifnif','tipoidfiscal'); 
+	}); 
+
+	$("select[name='tipoidfiscal']").change(function() {
+		$("input[name='cifnif']").val("");
+	   	$("button[type='submit']").attr("disabled",true);
+	}); 
+
+	auxscript='';
+
+	auxscript +='<label class="checkbox-inline" style="margin-left: 0;">'; 
+	auxscript +='<input type="checkbox" name="privpubli" value="TRUE" checked="" disabled="disabled">';
+	auxscript +='empresa privada (no publica)';
+	auxscript +='</label>';
+
+	$('input[name="cifnif"]').parent().append(auxscript);
+
+	$("input[name='personafisica']").change(function() {
+		if(!$("input[name='personafisica']").prop("checked") ) {
+
+			$("input[name='cifnif']").val("");
+
+		  	$("button[type='submit']").attr("disabled",true);
+		  	$("input[name='privpubli']").attr("disabled",false);
+		  	$("select[name='tipoidfiscal']").val("R.U.C");
+		  	$("select[name='tipoidfiscal']").attr("disabled",true);
+
+
+			$("input[name='privpubli']").change(function() {
+
+				$("input[name='cifnif']").val("");
+
+			  	$("button[type='submit']").attr("disabled",true);
+			}); 
+
+		}else{
+			$("input[name='cifnif']").val("");
+		  	$("button[type='submit']").attr("disabled",true);
+		  	$("input[name='privpubli']").attr("disabled",true);
+		  	$("select[name='tipoidfiscal']").val("Cedula");
+		  	$("select[name='tipoidfiscal']").attr("disabled",false);
+		}
+	});
+	
+}
+
+function funvalidar_nuevaventa(){
+
+	$("input[name='nuevo_cifnif']").keyup(function() {
+		validar($(this).val(),'nuevo_cifnif','nuevo_tipoidfiscal'); 
+	}); 
+
+	$("select[name='nuevo_tipoidfiscal']").change(function() {
+		$("input[name='nuevo_cifnif']").val("");
+	   	$("button[type='submit']").attr("disabled",true);
+	}); 
+
+	auxscript='';
+
+	auxscript +='<label class="checkbox-inline" style="margin-left: 0;">'; 
+	auxscript +='<input type="checkbox" name="privpubli" value="TRUE" checked="" disabled="disabled">';
+	auxscript +='empresa privada (no publica)';
+	auxscript +='</label>';
+
+	$('input[name="nuevo_cifnif"]').parent().append(auxscript);
+
+	$("input[name='personafisica']").change(function() {
+		if(!$("input[name='personafisica']").prop("checked") ) {
+
+			$("input[name='nuevo_cifnif']").val("");
+
+		  	$("button[type='submit']").attr("disabled",true);
+		  	$("input[name='privpubli']").attr("disabled",false);
+		  	$("select[name='nuevo_tipoidfiscal']").val("R.U.C");
+		  	$("select[name='nuevo_tipoidfiscal']").attr("disabled",true);
+
+
+			$("input[name='privpubli']").change(function() {
+
+				$("input[name='nuevo_cifnif']").val("");
+
+			  	$("button[type='submit']").attr("disabled",true);
+			}); 
+
+		}else{
+			$("input[name='nuevo_cifnif']").val("");
+		  	$("button[type='submit']").attr("disabled",true);
+		  	$("input[name='privpubli']").attr("disabled",true);
+		  	$("select[name='nuevo_tipoidfiscal']").val("Cedula");
+		  	$("select[name='nuevo_tipoidfiscal']").attr("disabled",false);
+		}
+	});
+	
+}
+
+function validar_chkpersonafisica(campocedrud, camposeltipofiscal){
+	if(!$("input[name='personafisica']").prop("checked") ) {
+
+		$("input[name='"+campocedrud+"']").val("");
+
+	  	$("button[type='submit']").attr("disabled",true);
+	  	$("input[name='privpubli']").attr("disabled",false);
+	  	$("select[name='"+camposeltipofiscal+"']").val("R.U.C");
+	  	$("select[name='"+camposeltipofiscal+"']").parent().append('<input type="hidden" name="aux" value="R.U.C">');
+	  	$("select[name='"+camposeltipofiscal+"']").attr("name",camposeltipofiscal+'_AUX');
+	  	$("input[name='aux']").attr("name",camposeltipofiscal);
+
+
+	  	$("select[name='"+camposeltipofiscal+"_AUX']").attr("disabled",true);
+
+
+		$("input[name='privpubli']").change(function() {
+
+
+		  	$("button[type='submit']").attr("disabled",true);
+		}); 
+
+	}else{
+	  	$("button[type='submit']").attr("disabled",true);
+	  	$("input[name='privpubli']").attr("disabled",true);
+	  	$("select[name='"+camposeltipofiscal+"']").val("Cedula");
+	  	$("select[name='"+camposeltipofiscal+"']").attr("disabled",false);
+	}
+}
+
+
+
 $(document).ready(function() {
 	var scriptaux ='';
 	var aux = '';
 	var aux2 = '';
-	var tipfiscal = '';
-	var perfisica = '';
 
-	tipfiscal = $('select[name="tipoidfiscal"]').val();
+	funvalidar();
 
+	funvalidar_nuevaventa();
+
+	validar_chkpersonafisica('nuevo_cifnif', 'nuevo_tipoidfiscal');
+
+	validar_chkpersonafisica('cifnif', 'tipoidfiscal');
 
 	aux2= "'cifnif'";
 	aux = 'input[name='+aux2+']';
@@ -54,8 +189,51 @@ $(document).ready(function() {
 	scriptaux += '<div class="scriptaux">';
 	scriptaux += '<script>';
 	scriptaux += '$("'+aux+'").keyup(function() {';
-	scriptaux += '	validar($(this).val(),"'+tipfiscal+'");';
-	scriptaux += '});';
+	scriptaux += '	validar($(this).val()); ';
+	scriptaux += '}); ';
+
+	aux2= "'tipoidfiscal'";
+	scriptaux += '$("select[name='+aux2+']").change(function() {';
+	aux2= "'cifnif'";
+	scriptaux += '	$("input[name='+aux2+']").val("");';
+	aux2= "'submit'";
+	scriptaux += '  $("button[type='+aux2+']").attr("disabled",true);';
+	scriptaux += '}); ';
+
+	aux2= "'personafisica'";
+	scriptaux += '$("input[name='+aux2+']").change(function() {';
+	scriptaux += 'if(!$("input[name='+aux2+']").prop("checked") ) {';
+	aux2= "'cifnif'";
+	scriptaux += '	$("input[name='+aux2+']").val("");';
+	aux2= "'submit'";
+	scriptaux += '  $("button[type='+aux2+']").attr("disabled",true);';
+	aux2= "'privpubli'";
+	scriptaux += '  $("input[name='+aux2+']").attr("disabled",false);';
+	aux2= "'tipoidfiscal'";
+	scriptaux += '  $("select[name='+aux2+']").val("R.U.C");';
+	scriptaux += '  $("select[name='+aux2+']").attr("disabled",true);';
+
+	aux2= "'privpubli'";
+	scriptaux += '$("input[name='+aux2+']").change(function() {';
+	aux2= "'cifnif'";
+	scriptaux += '	$("input[name='+aux2+']").val("");';
+	aux2= "'submit'";
+	scriptaux += '  $("button[type='+aux2+']").attr("disabled",true);';
+	scriptaux += '}); ';
+
+	scriptaux += '}else{';
+	aux2= "'cifnif'";
+	scriptaux += '	$("input[name='+aux2+']").val("");';
+	aux2= "'submit'";
+	scriptaux += '  $("button[type='+aux2+']").attr("disabled",true);';
+	aux2= "'privpubli'";
+	scriptaux += '  $("input[name='+aux2+']").attr("disabled",true);';
+	aux2= "'tipoidfiscal'";
+	scriptaux += '  $("select[name='+aux2+']").val("Cedula");';
+	scriptaux += '  $("select[name='+aux2+']").attr("disabled",false);';
+	scriptaux += '}';
+	scriptaux += '}); ';
+
 	scriptaux += '</script>';
 
 	$('#b_nuevo_cliente').click(function() {
